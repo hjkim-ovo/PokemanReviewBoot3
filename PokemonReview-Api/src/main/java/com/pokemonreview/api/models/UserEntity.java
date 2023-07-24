5aafa0ba1d1f954e7c28a1d14e87f87c11b36f9b
hjkim-ovo <smile_now@naver.com>
2023-07-24 17:43:01 +0900

    user등록

--- PokemonReview-Api/src/main/java/com/pokemonreview/api/models/UserEntity.java
+++ PokemonReview-Api/src/main/java/com/pokemonreview/api/models/UserEntity.java
@@ -22,7 +22,7 @@ public class UserEntity {
     private String username;
 
     private String password;
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "user_roles", 
             joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
